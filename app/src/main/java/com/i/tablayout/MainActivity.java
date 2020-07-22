190373ff23539f4c440d97e68d53ce45d160862e
inancyillmaz <inancyillmaz@gmail.com>
2020-07-22 19:47:04 +0300

    second commit
    Signed-off-by: inancyillmaz <inancyillmaz@gmail.com>

--- app/src/main/java/com/i/tablayout/MainActivity.java
+++ app/src/main/java/com/i/tablayout/MainActivity.java
@@ -25,9 +25,30 @@ public class MainActivity extends AppCompatActivity {
 
 
         adapter = new TabAdapter(getSupportFragmentManager());
-        adapter.addFragment(new Tab1Fragment(), "1.Frag");
-        adapter.addFragment(new Tab2Fragment(), "2.Frag");
-        adapter.addFragment(new Tab3Fragment(), "3.Frag");
+        adapter.addFragment(new Tab1Fragment(), "1.Fragment");
+        adapter.addFragment(new Tab2Fragment(), "2.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "3.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "4.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "5.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "6.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "7.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "8.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "9.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "10.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "11.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "12.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "13.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "14.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "15.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "16.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "17.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "18.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "19.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "20.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "21.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "22.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "23.Fragment");
+        adapter.addFragment(new Tab3Fragment(), "23.Fragment");
 
 
         viewPager.setAdapter(adapter);
